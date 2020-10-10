82cba42e3f4f3500a46b8d763a1ce81b090a4f5d
iwansafr <iwansafr@gmail.com>
2020-10-10 13:46:38 +0700

    home index

--- routes/web.php
+++ routes/web.php
@@ -25,9 +25,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index']);
 
 Route::middleware('auth')->group(function () {
     Route::prefix('/admin')->group(function () {
