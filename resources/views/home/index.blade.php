7d3e209091e423e82a5ae5a8ee41822234b2cb54
iwansafr <iwansafr@gmail.com>
2020-10-26 14:06:47 +0700

    footer custom

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -28,14 +28,14 @@
 
   <!-- Main Footer -->
   @include('home.part.footer')
-  <footer class="main-footer">
+  {{-- <footer class="main-footer"> --}}
     <!-- To the right -->
-    <div class="float-right d-none d-sm-inline">
+    {{-- <div class="float-right d-none d-sm-inline">
       Anything you want
     </div>
     <!-- Default to the left -->
-    <strong>Copyright &copy; 2014-2019 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
-  </footer>
+    <strong>Copyright &copy; 2014-2019 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved. --}}
+  {{-- </footer> --}}
 </div>
 <!-- ./wrapper -->
 
--- resources/views/home/part/footer.blade.php
+++ resources/views/home/part/footer.blade.php
@@ -2,7 +2,7 @@
   <footer style="min-height: 200px;">
     <div class="container">
       <div class="row p-3">
-        <div class="col-md-4">
+        <div class="col-md-3">
           <h5 class="text-uppercase pl-3">Need Help ?</h5>
           <ul class="list-unstyled">
             <li><a href="" class="text-light nav-link">About Us</a></li>
@@ -10,7 +10,7 @@
             <li><a href="" class="text-light nav-link">Contact</a></li>
           </ul>
         </div>
-        <div class="col-md-4">
+        <div class="col-md-3">
           <h5 class="text-uppercase pl-3">Customer Service</h5>
           <ul class="list-unstyled">
             <li><a href="" class="text-light nav-link">How It Works ?</a></li>
@@ -19,7 +19,16 @@
             <li><a href="" class="text-light nav-link">Testimonials</a></li>
           </ul>
         </div>
-        <div class="col-md-4">
+        <div class="col-md-3">
+          <h5 class="text-uppercase pl-3">Customer Service</h5>
+          <ul class="list-unstyled">
+            <li><a href="" class="text-light nav-link">How It Works ?</a></li>
+            <li><a href="" class="text-light nav-link">Order Status</a></li>
+            <li><a href="" class="text-light nav-link">Policies and FAQ</a></li>
+            <li><a href="" class="text-light nav-link">Testimonials</a></li>
+          </ul>
+        </div>
+        <div class="col-md-3">
           <h5 class="text-uppercase pl-3">Stay Connected</h5>
           <ul class="list-unstyled">
             <li><a href="" class="text-light nav-link"><i class="fab fa-facebook"></i> Facebook</a></li>
@@ -28,6 +37,16 @@
             <li><a href="" class="text-light nav-link"><i class="fab fa-instagram"></i> Instagram</a></li>
           </ul>
         </div>
+      </div>
+    </div>
+    <hr>
+    <div class="footer">
+      <div class="container pb-3">
+        <div class="float-right d-none d-sm-inline">
+          Negara : Indonesia | Singapura | Cina | Jepang | Thailand | Malaysia
+        </div>
+        <!-- Default to the left -->
+        <strong>Copyright &copy; 2020 <a class="text-light" href="https://esoftgreat.com">esoftgreat</a>.</strong> All rights reserved.
       </div>
     </div>
   </footer>
